315964545f527949e78ef87f29a7a4b0c34ab78b
Sean Zheng <blackhorseya@gmail.com>
2023-10-11 07:31:41 +0800

    feat: add user authentication methods to IUserBiz interface
    - Add `Login` method to the `IUserBiz` interface
    - Add `Signup` method to the `IUserBiz` interface
    - Add `Logout` method to the `IUserBiz` interface
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- entity/domain/user/biz/interface.go
+++ entity/domain/user/biz/interface.go
@@ -2,6 +2,19 @@
 
 package biz
 
+import (
+	userM "github.com/blackhorseya/monorepo-go/entity/domain/user/model"
+	"github.com/blackhorseya/monorepo-go/pkg/contextx"
+)
+
 // IUserBiz is a user biz interface
 type IUserBiz interface {
+	// Login serve caller to given username and password to login system
+	Login(ctx contextx.Contextx, username, password string) (account *userM.UserAccount, err error)
+
+	// Signup serve caller to given username and password to signup system
+	Signup(ctx contextx.Contextx, username, password string) (account *userM.UserAccount, err error)
+
+	// Logout serve caller to logout system
+	Logout(ctx contextx.Contextx, token string) error
 }
--- entity/domain/user/biz/mock_interface.go
+++ entity/domain/user/biz/mock_interface.go
@@ -5,6 +5,10 @@
 package biz
 
 import (
+	reflect "reflect"
+
+	model "github.com/blackhorseya/monorepo-go/entity/domain/user/model"
+	contextx "github.com/blackhorseya/monorepo-go/pkg/contextx"
 	gomock "go.uber.org/mock/gomock"
 )
 
@@ -29,4 +33,48 @@ func NewMockIUserBiz(ctrl *gomock.Controller) *MockIUserBiz {
 // EXPECT returns an object that allows the caller to indicate expected use.
 func (m *MockIUserBiz) EXPECT() *MockIUserBizMockRecorder {
 	return m.recorder
+}
+
+// Login mocks base method.
+func (m *MockIUserBiz) Login(ctx contextx.Contextx, username, password string) (*model.UserAccount, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "Login", ctx, username, password)
+	ret0, _ := ret[0].(*model.UserAccount)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// Login indicates an expected call of Login.
+func (mr *MockIUserBizMockRecorder) Login(ctx, username, password interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Login", reflect.TypeOf((*MockIUserBiz)(nil).Login), ctx, username, password)
+}
+
+// Logout mocks base method.
+func (m *MockIUserBiz) Logout(ctx contextx.Contextx, token string) error {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "Logout", ctx, token)
+	ret0, _ := ret[0].(error)
+	return ret0
+}
+
+// Logout indicates an expected call of Logout.
+func (mr *MockIUserBizMockRecorder) Logout(ctx, token interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Logout", reflect.TypeOf((*MockIUserBiz)(nil).Logout), ctx, token)
+}
+
+// Signup mocks base method.
+func (m *MockIUserBiz) Signup(ctx contextx.Contextx, username, password string) (*model.UserAccount, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "Signup", ctx, username, password)
+	ret0, _ := ret[0].(*model.UserAccount)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// Signup indicates an expected call of Signup.
+func (mr *MockIUserBizMockRecorder) Signup(ctx, username, password interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Signup", reflect.TypeOf((*MockIUserBiz)(nil).Signup), ctx, username, password)
 }
