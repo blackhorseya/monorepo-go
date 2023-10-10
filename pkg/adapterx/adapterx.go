6ad3ad26ee3b319e7b6116a608f58751b9bbfaad
Sean Zheng <blackhorseya@gmail.com>
2023-10-11 07:48:25 +0800

    feat: define interface for service adapters
    - Add a new file: `pkg/adapterx/adapterx.go`
    - Define the `Servicer` interface with `Start()` and `AwaitSignal()` methods
    - Define the `Restful` interface that extends `Servicer` and adds `InitRouting()` method
    - Define the `Grpc` interface that extends `Servicer`
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

---
+++ pkg/adapterx/adapterx.go
@@ -0,0 +1,23 @@
+package adapterx
+
+// Servicer is the interface that wraps the basic Serve method.
+type Servicer interface {
+	// Start a service asynchronously.
+	Start() error
+
+	// AwaitSignal waits for a signal to shutdown the service.
+	AwaitSignal() error
+}
+
+// Restful is the interface that wraps the restful api method
+type Restful interface {
+	Servicer
+
+	// InitRouting init the routing of restful api
+	InitRouting() error
+}
+
+// Grpc is the interface that wraps the grpc api method
+type Grpc interface {
+	Servicer
+}
