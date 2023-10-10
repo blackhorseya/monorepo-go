93aa4df954c22747e441dffa0a3deb04a2c6adf3
Sean Zheng <blackhorseya@gmail.com>
2023-10-11 06:52:11 +0800

    feat: add contextx package with helpful functions
    - Add `time` import to `pkg/contextx/contextx.go`
    - Add `Background` function to `pkg/contextx/contextx.go`
    - Add `WithLogger` function to `pkg/contextx/contextx.go`
    - Add `WithValue` function to `pkg/contextx/contextx.go`
    - Add `WithTimeout` function to `pkg/contextx/contextx.go`
    - Add `WithCancel` function to `pkg/contextx/contextx.go`
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- pkg/contextx/contextx.go
+++ pkg/contextx/contextx.go
@@ -2,6 +2,7 @@ package contextx
 
 import (
 	"context"
+	"time"
 
 	"go.uber.org/zap"
 )
@@ -10,4 +11,48 @@ import (
 type Contextx struct {
 	context.Context
 	*zap.Logger
+}
+
+// Background returns a non-nil, empty Contextx. It is never canceled, has no values, and has no deadline.
+func Background() Contextx {
+	return Contextx{
+		Context: context.Background(),
+		Logger:  zap.L(),
+	}
+}
+
+// WithLogger returns a copy of parent in which the value associated with key is val.
+func WithLogger(logger *zap.Logger) Contextx {
+	return Contextx{
+		Context: context.Background(),
+		Logger:  logger,
+	}
+}
+
+// WithValue returns a copy of parent in which the value associated with key is val.
+func WithValue(parent Contextx, key, val interface{}) Contextx {
+	return Contextx{
+		Context: context.WithValue(parent.Context, key, val),
+		Logger:  parent.Logger,
+	}
+}
+
+// WithTimeout returns a copy of the parent context with the timeout adjusted to be no later than d.
+func WithTimeout(parent Contextx, d time.Duration) (Contextx, context.CancelFunc) {
+	ctx, cancel := context.WithTimeout(parent.Context, d)
+
+	return Contextx{
+		Context: ctx,
+		Logger:  parent.Logger,
+	}, cancel
+}
+
+// WithCancel returns a copy of the parent context with a new Done channel.
+func WithCancel(parent Contextx) (Contextx, context.CancelFunc) {
+	ctx, cancel := context.WithCancel(parent.Context)
+
+	return Contextx{
+		Context: ctx,
+		Logger:  parent.Logger,
+	}, cancel
 }
