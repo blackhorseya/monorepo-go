16827297aac88bb60fff7a297ee47f87b278b444
Sean Zheng <blackhorseya@gmail.com>
2023-10-15 06:57:18 +0800

    feat: implement new logging functionality
    - Added a new file `logx.go` in the `internal/pkg/logx` directory
    - Implemented the `NewLoggerWithConfig` function in `logx.go`
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

---
+++ internal/pkg/logx/logx.go
@@ -0,0 +1,12 @@
+package logx
+
+import (
+	"github.com/blackhorseya/monorepo-go/internal/pkg/configx"
+	"go.uber.org/zap"
+)
+
+// NewLoggerWithConfig will create a new logger instance with config.
+func NewLoggerWithConfig(config *configx.Config) (logger *zap.Logger, err error) {
+	// todo: 2023/10/15|sean|impl me
+	panic("implement me")
+}
