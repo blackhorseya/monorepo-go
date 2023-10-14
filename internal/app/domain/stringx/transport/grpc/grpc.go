a844add6f758e1ee45d00acb2fffb8937c3b2f31
Sean Zheng <blackhorseya@gmail.com>
2023-10-14 10:09:06 +0800

    feat: implement grpc transport functions for stringx domain
    - Add `grpc` package for stringx transport in the domain
    - Implement `New` function for grpc transport
    - Implement `ToUpper` function in grpc transport
    - Implement `Count` function in grpc transport
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- internal/app/domain/stringx/transport/grpc/grpc.go
+++ internal/app/domain/stringx/transport/grpc/grpc.go
@@ -1,1 +1,31 @@
 package grpc
+
+import (
+	"context"
+
+	"github.com/blackhorseya/monorepo-go/entity/domain/stringx/model"
+	grpctransport "github.com/go-kit/kit/transport/grpc"
+	"github.com/go-kit/kit/transport/grpc/_grpc_test/pb"
+)
+
+type grpc struct {
+	pb.UnimplementedTestServer
+
+	toUpper grpctransport.Handler
+	count   grpctransport.Handler
+}
+
+// New grpc transport.
+func New() model.StringxServiceServer {
+	return &grpc{}
+}
+
+func (g *grpc) ToUpper(ctx context.Context, request *model.ToUpperRequest) (*model.ToUpperResponse, error) {
+	// todo: 2023/10/14|sean|impl me
+	panic("implement me")
+}
+
+func (g *grpc) Count(ctx context.Context, request *model.CountRequest) (*model.CountResponse, error) {
+	// todo: 2023/10/14|sean|impl me
+	panic("implement me")
+}
