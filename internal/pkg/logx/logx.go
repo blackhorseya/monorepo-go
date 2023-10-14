4ac7a75c89b8252fbcb72fb590643ab87ff11b19
Sean Zheng <blackhorseya@gmail.com>
2023-10-15 07:02:46 +0800

    feat: refactor logging system and improve configuration
    - Remove the panic and implement the NewLoggerWithConfig function
    - Unmarshal the log level from the config and return an error if it fails
    - Configure the console writer and encoder based on the log format
    - Create and configure the zap core
    - Replace the global logger with the new logger
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- internal/pkg/logx/logx.go
+++ internal/pkg/logx/logx.go
@@ -1,12 +1,37 @@
 package logx
 
 import (
+	"os"
+
 	"github.com/blackhorseya/monorepo-go/internal/pkg/configx"
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 // NewLoggerWithConfig will create a new logger instance with config.
 func NewLoggerWithConfig(config *configx.Config) (logger *zap.Logger, err error) {
-	// todo: 2023/10/15|sean|impl me
-	panic("implement me")
+	level := zap.NewAtomicLevel()
+	err = level.UnmarshalText([]byte(config.Log.Level))
+	if err != nil {
+		return nil, err
+	}
+
+	cw := zapcore.Lock(os.Stdout)
+	zapConfig := zap.NewDevelopmentEncoderConfig()
+	zapConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
+	enc := zapcore.NewConsoleEncoder(zapConfig)
+	if config.Log.Format == "json" {
+		zapConfig = zap.NewProductionEncoderConfig()
+		enc = zapcore.NewJSONEncoder(zapConfig)
+	}
+
+	cores := make([]zapcore.Core, 0)
+	cores = append(cores, zapcore.NewCore(enc, cw, level))
+
+	core := zapcore.NewTee(cores...)
+	ret := zap.New(core)
+
+	zap.ReplaceGlobals(ret)
+
+	return ret, nil
 }
