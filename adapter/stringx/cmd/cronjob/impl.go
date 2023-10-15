27ce3d0ed409d949ed51f50773b96b3fab81d35c
Sean Zheng <blackhorseya@gmail.com>
2023-10-15 08:05:04 +0800

    feat: refactor implementation to use goroutines and signal handling
    - Add `taskC` and `done` channels to the `impl` struct
    - Remove the panic and add a logger info statement in the `Start` function
    - Implement the logic for the `Start` function using a goroutine
    - Implement the logic for the `AwaitSignal` function using signal handling
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- adapter/stringx/cmd/cronjob/impl.go
+++ adapter/stringx/cmd/cronjob/impl.go
@@ -1,6 +1,11 @@
 package cronjob
 
 import (
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
 	"github.com/blackhorseya/monorepo-go/internal/pkg/configx"
 	"github.com/blackhorseya/monorepo-go/pkg/adapterx"
 	"github.com/spf13/viper"
@@ -11,6 +16,9 @@ type impl struct {
 	viper  *viper.Viper
 	config *configx.Config
 	logger *zap.Logger
+
+	taskC chan time.Time
+	done  chan struct{}
 }
 
 func newImpl(viper *viper.Viper, config *configx.Config, logger *zap.Logger) adapterx.Servicer {
@@ -18,15 +26,49 @@ func newImpl(viper *viper.Viper, config *configx.Config, logger *zap.Logger) ada
 		viper:  viper,
 		config: config,
 		logger: logger.With(zap.String("type", "cronjob")),
+		taskC:  make(chan time.Time, 1),
+		done:   make(chan struct{}, 0),
 	}
 }
 
 func (i *impl) Start() error {
-	// todo: 2023/10/12|sean|impl me
-	panic("implement me")
+	i.logger.Info("start cronjob")
+
+	go func() {
+		// todo: 2023/10/15|sean|pass config to cronjob
+		ticker := time.NewTicker(1 * time.Second)
+
+		for {
+			select {
+			case now := <-ticker.C:
+				select {
+				case i.taskC <- now:
+				case <-time.After(50 * time.Millisecond):
+					return
+				}
+			case <-i.done:
+				i.logger.Info("stop cronjob")
+				ticker.Stop()
+				return
+			case t := <-i.taskC:
+				i.logger.Debug("do cronjob", zap.Time("trigger_at", t))
+			}
+		}
+	}()
+
+	return nil
 }
 
 func (i *impl) AwaitSignal() error {
-	// todo: 2023/10/12|sean|impl me
-	panic("implement me")
+	c := make(chan os.Signal, 1)
+	signal.Reset(syscall.SIGTERM, syscall.SIGINT)
+	signal.Notify(c, syscall.SIGTERM, syscall.SIGINT)
+
+	if sig := <-c; true {
+		i.logger.Info("receive signal", zap.String("signal", sig.String()))
+
+		i.done <- struct{}{}
+	}
+
+	return nil
 }
