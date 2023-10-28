9a02452828bb54b8bcefe667aefd089d8dcdc454
Sean Zheng <blackhorseya@gmail.com>
2023-10-28 14:59:01 +0800

    feat: create new event domain implementation
    - Add new file `internal/app/domain/event/biz/BUILD.bazel`
    - Add new file `internal/app/domain/event/biz/impl.go`
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

---
+++ internal/app/domain/event/biz/impl.go
@@ -0,0 +1,26 @@
+package biz
+
+import (
+	"github.com/blackhorseya/monorepo-go/entity/domain/event/biz"
+	eventM "github.com/blackhorseya/monorepo-go/entity/domain/event/model"
+	userM "github.com/blackhorseya/monorepo-go/entity/domain/user/model"
+	"github.com/blackhorseya/monorepo-go/pkg/contextx"
+)
+
+type impl struct {
+}
+
+// New create a new event biz.
+func New() biz.IEventBiz {
+	return &impl{}
+}
+
+func (i *impl) CreateRedPacket(ctx contextx.Contextx, who *userM.UserAccount, amount uint64, count uint32) (packet *eventM.RedPacket, err error) {
+	// todo: 2023/10/28|sean|impl me
+	panic("implement me")
+}
+
+func (i *impl) GrabRedPacket(ctx contextx.Contextx, who *userM.UserAccount, packetID string) (amount uint64, err error) {
+	// todo: 2023/10/28|sean|impl me
+	panic("implement me")
+}
