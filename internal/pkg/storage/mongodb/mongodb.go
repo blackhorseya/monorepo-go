90bca5edc17522d7563bbebd5be79492d8712780
Sean Zheng <blackhorseya@gmail.com>
2023-10-30 07:32:23 +0800

    chore: add required Go modules and create MongoDB storage package
    - Added `github.com/go-sql-driver/mysql` as a required module in the `go.mod` file.
    - Added `github.com/pkg/errors`, `go.mongodb.org/mongo-driver`, `github.com/golang/snappy`, `github.com/klauspost/compress`, `github.com/montanaflynn/stats`, `github.com/xdg-go/pbkdf2`, `github.com/xdg-go/scram`, `github.com/xdg-go/stringprep`, and `github.com/youmark/pkcs8` as required modules in the `go.mod` file.
    - Created a new file `internal/pkg/storage/mongodb/mongodb.go` with the `NewClient` function.
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

---
+++ internal/pkg/storage/mongodb/mongodb.go
@@ -0,0 +1,11 @@
+package mongodb
+
+import (
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+// NewClient returns a new mongo client.
+func NewClient() (*mongo.Client, error) {
+	// todo: 2023/10/30|sean|implement me
+	panic("implement me")
+}
