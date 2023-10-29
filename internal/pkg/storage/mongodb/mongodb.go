31f3592ff86d905c8f3433ed6d9a7840c06cbbf9
Sean Zheng <blackhorseya@gmail.com>
2023-10-30 07:36:24 +0800

    chore: implement NewClient function and add dependencies to BUILD.bazel
    - Add dependencies to `BUILD.bazel`
    - Implement `NewClient` function in `mongodb.go`
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- internal/pkg/storage/mongodb/mongodb.go
+++ internal/pkg/storage/mongodb/mongodb.go
@@ -1,11 +1,21 @@
 package mongodb
 
 import (
+	"github.com/blackhorseya/monorepo-go/internal/pkg/configx"
+	"github.com/blackhorseya/monorepo-go/pkg/contextx"
+	"github.com/pkg/errors"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 // NewClient returns a new mongo client.
-func NewClient() (*mongo.Client, error) {
-	// todo: 2023/10/30|sean|implement me
-	panic("implement me")
+func NewClient(config *configx.Config) (*mongo.Client, error) {
+	opts := options.Client().ApplyURI(config.Storage.DSN)
+
+	client, err := mongo.Connect(contextx.Background(), opts)
+	if err != nil {
+		return nil, errors.Wrap(err, "connect mongodb")
+	}
+
+	return client, nil
 }
