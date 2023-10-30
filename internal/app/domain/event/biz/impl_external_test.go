9cffdcf3bcf7b78e652f85370dd67eb4cf43c6db
Sean Zheng <blackhorseya@gmail.com>
2023-10-30 09:15:25 +0800

    feat: refactor red packet creation and error handling
    - Remove a commented out todo and skip external test
    - Create a red packet using the event.CreateRedPacket function
    - Log the created packet using ctx.Debug
    - Check if the packet Id is empty and return an error if it is
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- internal/app/domain/event/biz/impl_external_test.go
+++ internal/app/domain/event/biz/impl_external_test.go
@@ -4,10 +4,42 @@ package biz_test
 
 import (
 	"testing"
+
+	userM "github.com/blackhorseya/monorepo-go/entity/domain/user/model"
+	"github.com/blackhorseya/monorepo-go/internal/app/domain/event/biz"
+	"github.com/blackhorseya/monorepo-go/internal/app/domain/event/biz/repo/memory"
+	"github.com/blackhorseya/monorepo-go/pkg/contextx"
+	"github.com/google/uuid"
+	"go.uber.org/zap"
 )
 
 func TestImpl_CreateRedPacket(t *testing.T) {
-	// todo: 2023/10/30|sean|implement this test
+	logger := zap.NewExample()
+	storage := memory.New()
+	event := biz.New(storage)
 
-	t.Skip("skip external test")
+	ctx := contextx.WithLogger(logger)
+
+	tester := &userM.UserAccount{
+		Id:          uuid.New().String(),
+		Username:    "tester",
+		Email:       "tester@gmail.com",
+		Password:    "",
+		AccessToken: "",
+		IdToken:     "",
+		Profile:     nil,
+	}
+
+	packet, err := event.CreateRedPacket(ctx, tester, 100, 10)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	ctx.Debug("got packet", zap.Any("packet", &packet))
+
+	if packet.Id == "" {
+		t.Error("packet id should not be empty")
+		return
+	}
 }
