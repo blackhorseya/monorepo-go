58a2afb50eb8ebfefff5c6370e9b85d9c040e898
Sean Zheng <blackhorseya@gmail.com>
2023-11-01 08:30:35 +0800

    feat: add RESTful API endpoints for creating and listing red packets
    - Added a new file `restful.go` in the `internal/app/domain/event/transports/restful` directory
    - Created a function `MakeCreateRedPacketHandler` that returns an `http.Handler` and is currently throwing a panic
    - Created a function `MakeListRedPacketHandler` that returns an `http.Handler` and is currently throwing a panic
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

---
+++ internal/app/domain/event/transports/restful/restful.go
@@ -0,0 +1,20 @@
+package restful
+
+import (
+	"net/http"
+
+	"github.com/blackhorseya/monorepo-go/pkg/contextx"
+	"github.com/go-kit/kit/endpoint"
+)
+
+// MakeCreateRedPacketHandler create red packet handler.
+func MakeCreateRedPacketHandler(ctx contextx.Contextx, endpoint endpoint.Endpoint) http.Handler {
+	// todo: 2023/11/1|sean|implement me
+	panic("implement me")
+}
+
+// MakeListRedPacketHandler list red packet handler.
+func MakeListRedPacketHandler(ctx contextx.Contextx, endpoint endpoint.Endpoint) http.Handler {
+	// todo: 2023/11/1|sean|implement me
+	panic("implement me")
+}
