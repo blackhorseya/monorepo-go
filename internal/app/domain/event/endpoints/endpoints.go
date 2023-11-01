e45777f9a248eec98bf48c2ab61e695905147935
Sean Zheng <blackhorseya@gmail.com>
2023-11-01 08:04:21 +0800

    feat: implement CreateRedPacket endpoint in endpoints.go
    - Import the package `github.com/blackhorseya/monorepo-go/entity/domain/user/model` in `internal/app/domain/event/endpoints/endpoints.go`
    - Define the struct `CreateRedPacketRequest` with fields `Who`, `Amount`, and `Count` in `internal/app/domain/event/endpoints/endpoints.go`
    - Define the struct `CreateRedPacketResponse` with the `Response` field in `internal/app/domain/event/endpoints/endpoints.go`
    - Implement the `MakeCreateRedPacketEndpoint` function in `internal/app/domain/event/endpoints/endpoints.go`
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- internal/app/domain/event/endpoints/endpoints.go
+++ internal/app/domain/event/endpoints/endpoints.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 
+	userM "github.com/blackhorseya/monorepo-go/entity/domain/user/model"
 	"github.com/blackhorseya/monorepo-go/pkg/response"
 	"github.com/go-kit/kit/endpoint"
 )
@@ -24,5 +25,25 @@ func MakeListRedPacketEndpoint() endpoint.Endpoint {
 	return func(_ context.Context, request interface{}) (response interface{}, err error) {
 		// todo: 2023/10/30|sean|implement list red packet endpoint
 		return ListRedPacketResponse{}, errors.New("implement me")
+	}
+}
+
+// CreateRedPacketRequest create red packet request struct.
+type CreateRedPacketRequest struct {
+	Who    *userM.UserAccount `json:"who,omitempty"`
+	Amount uint64             `json:"amount,omitempty"`
+	Count  uint32             `json:"count,omitempty"`
+}
+
+// CreateRedPacketResponse create red packet response struct.
+type CreateRedPacketResponse struct {
+	response.Response `json:",inline"`
+}
+
+// MakeCreateRedPacketEndpoint make create red packet endpoint.
+func MakeCreateRedPacketEndpoint() endpoint.Endpoint {
+	return func(_ context.Context, request interface{}) (response interface{}, err error) {
+		// todo: 2023/11/1|sean|implement create red packet endpoint
+		return CreateRedPacketResponse{}, errors.New("implement me")
 	}
 }
