79402e55b79eefb7850063473dc7523bdc6586a9
Sean Zheng <blackhorseya@gmail.com>
2023-11-01 08:32:45 +0800

    refactor: refactor code by removing TODO comments and panic statements
    - Remove the TODO comments and panic statements
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- internal/app/domain/event/transports/restful/restful.go
+++ internal/app/domain/event/transports/restful/restful.go
@@ -5,16 +5,15 @@ import (
 
 	"github.com/blackhorseya/monorepo-go/pkg/contextx"
 	"github.com/go-kit/kit/endpoint"
+	httptransport "github.com/go-kit/kit/transport/http"
 )
 
 // MakeCreateRedPacketHandler create red packet handler.
 func MakeCreateRedPacketHandler(ctx contextx.Contextx, endpoint endpoint.Endpoint) http.Handler {
-	// todo: 2023/11/1|sean|implement me
-	panic("implement me")
+	return httptransport.NewServer(endpoint, decodeCreateRedPacketRequest, encodeResponse)
 }
 
 // MakeListRedPacketHandler list red packet handler.
 func MakeListRedPacketHandler(ctx contextx.Contextx, endpoint endpoint.Endpoint) http.Handler {
-	// todo: 2023/11/1|sean|implement me
-	panic("implement me")
+	return httptransport.NewServer(endpoint, decodeListRedPacketRequest, encodeResponse)
 }
