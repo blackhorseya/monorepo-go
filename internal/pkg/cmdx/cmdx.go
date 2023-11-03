cd8ab18316102cb37bd221c66a331b4d40f48108
Sean Zheng <blackhorseya@gmail.com>
2023-11-03 19:54:36 +0800

    feat: add new `cobra.Command` for `ServiceCmd` in `cmdx.go`
    - Add a new file `internal/pkg/cmdx/BUILD.bazel`
    - Add a new `go_library` target in `internal/pkg/cmdx/BUILD.bazel`
    - Add `cmdx.go` in `internal/pkg/cmdx/cmdx.go`
    - Add a new import for `github.com/spf13/cobra` in `cmdx.go`
    - Add a new method `NewCmd` in `ServiceCmd` struct in `cmdx.go`
    - Add a new `cobra.Command` in the `NewCmd` method
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- internal/pkg/cmdx/cmdx.go
+++ internal/pkg/cmdx/cmdx.go
@@ -2,6 +2,7 @@ package cmdx
 
 import (
 	"github.com/blackhorseya/monorepo-go/pkg/adapterx"
+	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
 
@@ -10,4 +11,23 @@ type ServiceCmd struct {
 	Use   string
 	Short string
 	Run   func(v *viper.Viper) (adapterx.Servicer, error)
+}
+
+func (s *ServiceCmd) NewCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:   s.Use,
+		Short: s.Short,
+		Run: func(cmd *cobra.Command, args []string) {
+			v := viper.GetViper()
+
+			service, err := s.Run(v)
+			cobra.CheckErr(err)
+
+			err = service.Start()
+			cobra.CheckErr(err)
+
+			err = service.AwaitSignal()
+			cobra.CheckErr(err)
+		},
+	}
 }
