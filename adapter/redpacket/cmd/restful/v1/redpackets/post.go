f08b38e89e381bf004af740dd917f2d94fc1cf04
Sean Zheng <blackhorseya@gmail.com>
2023-11-03 20:31:41 +0800

    feat: add dependencies and functions for handling red packet creation
    - Add dependencies to the `BUILD.bazel` file for `internal/app/domain/event/endpoints`, `pkg/contextx`, `pkg/response`, `@com_github_go_kit_kit//endpoint`, and `@com_github_go_kit_kit//transport/http`
    - Add a new function `decodeCreateRedPacketRequest` in `post.go` that decodes the request body into a `CreateRedPacketRequest` struct
    - Add a new function `MakeCreateRedPacketHandler` in `post.go` that creates a red packet handler using the provided `endpoint` and uses `decodeCreateRedPacketRequest` and `response.EncodeJSON` as parameters for `httptransport.NewServer`
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- adapter/redpacket/cmd/restful/v1/redpackets/post.go
+++ adapter/redpacket/cmd/restful/v1/redpackets/post.go
@@ -1,1 +1,28 @@
 package redpackets
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+
+	"github.com/blackhorseya/monorepo-go/internal/app/domain/event/endpoints"
+	"github.com/blackhorseya/monorepo-go/pkg/contextx"
+	"github.com/blackhorseya/monorepo-go/pkg/response"
+	"github.com/go-kit/kit/endpoint"
+	httptransport "github.com/go-kit/kit/transport/http"
+)
+
+func decodeCreateRedPacketRequest(_ context.Context, r *http.Request) (interface{}, error) {
+	var req endpoints.CreateRedPacketRequest
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		return nil, err
+	}
+
+	return req, nil
+}
+
+// MakeCreateRedPacketHandler create red packet handler.
+func MakeCreateRedPacketHandler(ctx contextx.Contextx, endpoint endpoint.Endpoint) http.Handler {
+	return httptransport.NewServer(endpoint, decodeCreateRedPacketRequest, response.EncodeJSON)
+}
