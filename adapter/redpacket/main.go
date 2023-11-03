dd9bb2d5a8492502010d02fea4c964b00c5a6b80
Sean Zheng <blackhorseya@gmail.com>
2023-11-03 19:37:02 +0800

    feat: add redpacket API documentation and main file
    - Add a new file `adapter/redpacket/BUILD.bazel`
    - Add a new file `adapter/redpacket/api/docs/BUILD.bazel`
    - Add a new file `adapter/redpacket/api/docs/docs.go`
    - Add a new file `adapter/redpacket/api/docs/swagger.json`
    - Add a new file `adapter/redpacket/api/docs/swagger.yaml`
    - Add a new file `adapter/redpacket/main.go`
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

---
+++ adapter/redpacket/main.go
@@ -0,0 +1,13 @@
+package main
+
+// @title RedPacket API
+// @version 0.1.0
+// @description This is a sample server for redpacket.
+//
+// @contact.name Sean Zheng
+// @contact.email blackhorseya@gmail.com
+// @contact.url https://blog.seancheng.space
+//
+// @BasePath /api
+func main() {
+}
