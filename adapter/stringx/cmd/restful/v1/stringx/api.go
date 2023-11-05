66b7c2ae9da13ba11fbd78fc9d2ee8169738c3fb
Sean Zheng <blackhorseya@gmail.com>
2023-11-05 09:56:33 +0800

    feat: refactor stringx API endpoint creation
    - Add two new endpoints `/uppercase` and `/count` to the stringx API
    - Wrap the handlers for the new endpoints with `gin.WrapH`
    - Move the endpoint creation logic to separate files
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- adapter/stringx/cmd/restful/v1/stringx/api.go
+++ adapter/stringx/cmd/restful/v1/stringx/api.go
@@ -2,8 +2,15 @@ package stringx
 
 import (
 	"github.com/blackhorseya/monorepo-go/entity/domain/stringx/biz"
+	"github.com/blackhorseya/monorepo-go/internal/app/domain/stringx/endpoints"
+	"github.com/blackhorseya/monorepo-go/pkg/contextx"
 	"github.com/gin-gonic/gin"
 )
 
 func Handle(g *gin.RouterGroup, svc biz.IStringBiz) {
+	// register POST /uppercase
+	g.POST("/uppercase", gin.WrapH(MakeUppercaseHandler(contextx.Background(), endpoints.MakeUppercaseEndpoint(svc))))
+
+	// register POST /count
+	g.POST("/count", gin.WrapH(MakeCountHandler(contextx.Background(), endpoints.MakeCountEndpoint(svc))))
 }
