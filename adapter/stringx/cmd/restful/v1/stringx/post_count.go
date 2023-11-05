33e8f8ec69ba706273d14e1b7691028842a7861f
Sean Zheng <blackhorseya@gmail.com>
2023-11-05 09:54:57 +0800

    chore: refactor stringx RESTful transport layer
    - Modify the file `adapter/stringx/cmd/restful/v1/stringx/post_count.go`
    - Delete the file `internal/app/domain/stringx/transport/restful/BUILD.bazel`
    - Delete the file `internal/app/domain/stringx/transport/restful/model.go`
    - Delete the file `internal/app/domain/stringx/transport/restful/restful.go`
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- adapter/stringx/cmd/restful/v1/stringx/post_count.go
+++ adapter/stringx/cmd/restful/v1/stringx/post_count.go
@@ -1,1 +1,37 @@
 package stringx
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+
+	"github.com/blackhorseya/monorepo-go/internal/app/domain/stringx/endpoints"
+	"github.com/blackhorseya/monorepo-go/pkg/contextx"
+	"github.com/blackhorseya/monorepo-go/pkg/response"
+	"github.com/go-kit/kit/endpoint"
+	httptransport "github.com/go-kit/kit/transport/http"
+)
+
+func decodeCountRequest(ctx context.Context, r *http.Request) (interface{}, error) {
+	var req endpoints.CountRequest
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		return nil, err
+	}
+
+	return req, nil
+}
+
+// MakeCountHandler count handler.
+// @Summary count
+// @Description count
+// @Tags String
+// @Accept json
+// @Produce json
+// @Param request body endpoints.CountRequest true "count request"
+// @Success 200 {object} endpoints.CountResponse
+// @Failure 500 {object} endpoints.CountResponse
+// @Router /v1/string/count [post]
+func MakeCountHandler(ctx contextx.Contextx, endpoint endpoint.Endpoint) http.Handler {
+	return httptransport.NewServer(endpoint, decodeCountRequest, response.EncodeJSON)
+}
