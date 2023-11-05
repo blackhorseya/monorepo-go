0f4aefe3bc950b463f4af3388109012d0e13f26d
Sean Zheng <blackhorseya@gmail.com>
2023-11-05 09:51:12 +0800

    feat: refactor RESTful API implementation in stringx package
    - Added a new file `adapter/stringx/cmd/restful/v1/BUILD.bazel`
    - Added a new file `adapter/stringx/cmd/restful/v1/api.go`
    - Modified the file `adapter/stringx/cmd/restful/v1/BUILD.bazel`
    - Modified the file `adapter/stringx/cmd/restful/v1/stringx/BUILD.bazel`
    - Added a new file `adapter/stringx/cmd/restful/v1/stringx/api.go`
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

---
+++ adapter/stringx/cmd/restful/v1/stringx/api.go
@@ -0,0 +1,9 @@
+package stringx
+
+import (
+	"github.com/blackhorseya/monorepo-go/entity/domain/stringx/biz"
+	"github.com/gin-gonic/gin"
+)
+
+func Handle(g *gin.RouterGroup, svc biz.IStringBiz) {
+}
