e4b98a29686f22bbfea5c5c360cf79467d401652
Sean Zheng <blackhorseya@gmail.com>
2023-11-07 09:40:11 +0800

    chore: refactor file dependencies and initialization in lib/index.js and v1/BUILD.bazel
    - Add `adapter/redpacket/cmd/restful/v1:restful` as a dependency in `lib/index.js`
    - Add `entity/domain/event/biz`, `internal/app/domain/event/biz`, and `internal/app/domain/event/repo/memory` as dependencies in `lib/index.js`
    - Move the `octokit` initialization to a separate file in `lib/index.js`
    - Add `entity/domain/event/biz` as a dependency in `lib/index.js`
    - Add `eventB.IEventBiz` as a parameter in the `newImpl` function in `lib/index.js`
    - Add `eventB.IEventBiz` as a parameter in the `v1.Handle` function in `impl.go`
    - Add `eventB.IEventBiz` as a parameter in the `v1.Handle` function in `api.go`
    - Add `entity/domain/event/biz` as a dependency in `v1/BUILD.bazel`
    - Add `entity/domain/event/biz` as a dependency in `v1/api.go`
    - Add `entity/domain/event/biz` as a dependency in `v1/redpackets/BUILD.bazel`
    - Add `entity/domain/event/biz` as a dependency in `v1/redpackets/api.go`
    - Add `biz.New` and `memory.New` as dependencies in `wire.go`
    - Add `biz.New` and `memory.New` as dependencies in `wire_gen.go`
    - Add `entity/domain/event/b
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- adapter/redpacket/cmd/restful/wire_gen.go
+++ adapter/redpacket/cmd/restful/wire_gen.go
@@ -7,11 +7,17 @@
 package restful
 
 import (
+	"github.com/blackhorseya/monorepo-go/internal/app/domain/event/biz"
+	"github.com/blackhorseya/monorepo-go/internal/app/domain/event/repo/memory"
 	"github.com/blackhorseya/monorepo-go/internal/pkg/configx"
 	"github.com/blackhorseya/monorepo-go/internal/pkg/logx"
 	"github.com/blackhorseya/monorepo-go/pkg/adapterx"
 	"github.com/google/wire"
 	"github.com/spf13/viper"
+)
+
+import (
+	_ "github.com/blackhorseya/monorepo-go/adapter/redpacket/api/docs"
 )
 
 // Injectors from wire.go:
@@ -25,10 +31,12 @@ func New(v *viper.Viper) (adapterx.Servicer, error) {
 	if err != nil {
 		return nil, err
 	}
-	servicer := newImpl(config, logger)
+	storager := memory.New()
+	iEventBiz := biz.New(storager)
+	servicer := newImpl(config, logger, iEventBiz)
 	return servicer, nil
 }
 
 // wire.go:
 
-var providerSet = wire.NewSet(configx.NewWithViper, logx.NewWithConfig, newImpl)
+var providerSet = wire.NewSet(configx.NewWithViper, logx.NewWithConfig, newImpl, biz.New, memory.New)
--- internal/app/domain/event/endpoints/endpoints.go
+++ internal/app/domain/event/endpoints/endpoints.go
@@ -4,7 +4,9 @@ import (
 	"context"
 	"errors"
 
+	eventB "github.com/blackhorseya/monorepo-go/entity/domain/event/biz"
 	"github.com/blackhorseya/monorepo-go/entity/domain/user/model"
+	"github.com/blackhorseya/monorepo-go/pkg/contextx"
 	"github.com/blackhorseya/monorepo-go/pkg/response"
 	"github.com/go-kit/kit/endpoint"
 )
@@ -17,14 +19,28 @@ type ListRedPacketRequest struct {
 
 // ListRedPacketResponse list red packet response struct.
 type ListRedPacketResponse struct {
-	response.Response `json:",inline"`
+	*response.Response `json:",inline"`
 }
 
 // MakeListRedPacketEndpoint make list red packet endpoint.
-func MakeListRedPacketEndpoint() endpoint.Endpoint {
-	return func(_ context.Context, request interface{}) (response interface{}, err error) {
-		// todo: 2023/10/30|sean|implement list red packet endpoint
-		return ListRedPacketResponse{}, errors.New("implement me")
+func MakeListRedPacketEndpoint(svc eventB.IEventBiz) endpoint.Endpoint {
+	return func(_ context.Context, request interface{}) (resp interface{}, err error) {
+		req, _ := request.(ListRedPacketRequest)
+		ctx := contextx.Background()
+
+		ret, err := svc.ListRedPacket(ctx, eventB.ListRedPacketCondition{
+			Page:    req.Page,
+			PerPage: req.Size,
+		})
+		if err != nil {
+			return ListRedPacketResponse{
+				Response: &response.Response{Message: err.Error()},
+			}, err
+		}
+
+		return ListRedPacketResponse{
+			Response: response.OK.WithData(ret),
+		}, nil
 	}
 }
 
