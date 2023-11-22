70b5f8bb5a04b59cedebdcee809af270e8b85cc8
Sean Zheng <blackhorseya@gmail.com>
2023-11-22 10:50:35 +0800

    feat: add new channels and goroutines for task processing
    - Add `//pkg/contextx` to the list of go library imports in `adapter/redpacket/cmd/cronjob/BUILD.bazel`
    - Add `github.com/blackhorseya/monorepo-go/pkg/contextx` to the list of imports in `adapter/redpacket/cmd/cronjob/impl.go`
    - Add a new channel `taskC` and a new channel `done` to the `impl` struct in `adapter/redpacket/cmd/cronjob/impl.go`
    - Create a goroutine to produce tasks and send them to the `taskC` channel
    - Create a goroutine to consume tasks from the `taskC` channel and log a message
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- adapter/redpacket/cmd/cronjob/impl.go
+++ adapter/redpacket/cmd/cronjob/impl.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/blackhorseya/monorepo-go/internal/pkg/configx"
 	"github.com/blackhorseya/monorepo-go/pkg/adapterx"
+	"github.com/blackhorseya/monorepo-go/pkg/contextx"
 	"github.com/spf13/viper"
 	"go.uber.org/zap"
 )
@@ -17,6 +18,7 @@ type impl struct {
 	logger *zap.Logger
 
 	interval time.Duration
+	taskC    chan contextx.Contextx
 	done     chan struct{}
 }
 
@@ -25,13 +27,46 @@ func newCronjob(v *viper.Viper, config *configx.Config, logger *zap.Logger) (ada
 		config:   config,
 		logger:   logger,
 		interval: v.GetDuration("interval"),
+		taskC:    make(chan contextx.Contextx, 1),
+		done:     make(chan struct{}),
 	}, nil
 }
 
 func (i *impl) Start() error {
 	i.logger.Info("cronjob service start", zap.Duration("interval", i.interval))
 
-	// todo: 2023/11/22|sean|impl me
+	ticker := time.NewTicker(i.interval)
+
+	// produce task
+	go func() {
+		for {
+			select {
+			case <-i.done:
+				break
+			case <-ticker.C:
+				ctx := contextx.WithLogger(i.logger)
+
+				select {
+				case i.taskC <- ctx:
+				case <-time.After(50 * time.Millisecond):
+					ctx.Warn("task channel is full then drop task")
+				}
+			}
+		}
+	}()
+
+	// consume task
+	go func() {
+		for {
+			select {
+			case <-i.done:
+				break
+			case ctx := <-i.taskC:
+				ctx.Info("receive task")
+			}
+		}
+	}()
+
 	return nil
 }
 
