f6f3ee0f5f5f4d9d60b1160a1bf294323b450b62
Sean Zheng <blackhorseya@gmail.com>
2024-01-09 06:12:58 +0800

    refactor: implement graceful shutdown and logging for server
    - Remove the `todo` comments and replace them with proper implementation
    - Add logging statements for starting and receiving signals
    - Implement graceful shutdown of the server
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- adapter/shortenurl/cmd/restful/impl.go
+++ adapter/shortenurl/cmd/restful/impl.go
@@ -1,22 +1,69 @@
 package restful
 
 import (
+	"errors"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/blackhorseya/monorepo-go/internal/pkg/configx"
 	"github.com/blackhorseya/monorepo-go/pkg/adapterx"
+	"github.com/blackhorseya/monorepo-go/pkg/contextx"
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
+	"go.uber.org/zap"
 )
 
 type impl struct {
+	viper  *viper.Viper
+	config *configx.Config
+	logger *zap.Logger
+
+	router *gin.Engine
+	server *http.Server
 }
 
 func (i *impl) Start() error {
-	// todo: 2024/1/9|sean|implement me
-	panic("implement me")
+	go func() {
+		i.logger.Info("start restful service", zap.String("addr", i.server.Addr))
+
+		err := i.server.ListenAndServe()
+		if err != nil && !errors.Is(err, http.ErrServerClosed) {
+			i.logger.Fatal("restful service error", zap.Error(err))
+		}
+	}()
+
+	return nil
 }
 
 func (i *impl) AwaitSignal() error {
-	// todo: 2024/1/9|sean|implement me
-	panic("implement me")
+	c := make(chan os.Signal, 1)
+	signal.Reset(syscall.SIGTERM, syscall.SIGINT)
+	signal.Notify(c, syscall.SIGTERM, syscall.SIGINT)
+
+	if sig := <-c; true {
+		i.logger.Info("receive signal", zap.String("signal", sig.String()))
+
+		timeout, cancelFunc := contextx.WithTimeout(contextx.Background(), 5*time.Second)
+		defer cancelFunc()
+
+		err := i.server.Shutdown(timeout)
+		if err != nil {
+			i.logger.Error("shutdown restful server error", zap.Error(err))
+		}
+	}
+
+	return nil
 }
 
 func newService() (adapterx.Servicer, error) {
-	return &impl{}, nil
+	return &impl{
+		viper:  nil,
+		config: nil,
+		logger: nil,
+		router: nil,
+		server: nil,
+	}, nil
 }
