439a510aa1df0dab95460dad3b745f9dd9261437
Sean Zheng <blackhorseya@gmail.com>
2024-01-09 07:09:39 +0800

    feat: add memory repository implementation for URL shortening
    - Add new file `internal/app/domain/shortening/repo/memory/BUILD.bazel` with a new `go_library` target
    - Add new file `internal/app/domain/shortening/repo/memory/impl.go` with a new `impl` struct and two methods: `GetURLRecordByShortURL` and `CreateURLRecord`
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

---
+++ internal/app/domain/shortening/repo/memory/impl.go
@@ -0,0 +1,25 @@
+package memory
+
+import (
+	"github.com/blackhorseya/monorepo-go/entity/domain/shortening/model"
+	"github.com/blackhorseya/monorepo-go/internal/app/domain/shortening/repo"
+	"github.com/blackhorseya/monorepo-go/pkg/contextx"
+)
+
+type impl struct {
+}
+
+// NewStorager is used to create a new shortening storage instance.
+func NewStorager() repo.Storager {
+	return &impl{}
+}
+
+func (i *impl) GetURLRecordByShortURL(ctx contextx.Contextx, shortURL string) (record *model.ShortenedUrl, err error) {
+	// todo: 2024/1/9|sean|implement me
+	panic("implement me")
+}
+
+func (i *impl) CreateURLRecord(ctx contextx.Contextx, record *model.ShortenedUrl) error {
+	// todo: 2024/1/9|sean|implement me
+	panic("implement me")
+}
