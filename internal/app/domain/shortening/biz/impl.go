4503eab35c5f07c36af7c31fe8b23ad1b5b9664f
Sean Zheng <blackhorseya@gmail.com>
2024-01-09 07:48:16 +0800

    chore: update dependencies in `internal/app/domain/shortening/biz` and `internal/app/domain/shortening/repo`
    - Add `//internal/app/domain/shortening/biz` to the `go_library` deps in `internal/app/domain/shortening/biz/BUILD.bazel`
    - Add `//internal/app/domain/shortening/repo` to the `go_library` deps in `internal/app/domain/shortening/biz/BUILD.bazel`
    - Add `@org_golang_google_protobuf//types/known/timestamppb` to the `go_library` deps in `internal/app/domain/shortening/biz/BUILD.bazel`
    - Add `@org_uber_go_zap//:zap` to the `go_library` deps in `internal/app/domain/shortening/biz/BUILD.bazel`
    - Add `:biz` to the `deps` in `internal/app/domain/shortening/biz/biz_test.go go_test`
    - Add `//entity/domain/shortening/biz` to the `deps` in `internal/app/domain/shortening/biz/biz_test.go go_test`
    - Add `//entity/domain/shortening/model` to the `deps` in `internal/app/domain/shortening/biz/biz_test.go go_test`
    - Add `//internal/app/domain/shortening/repo` to the `deps` in `internal/app/domain/shortening/biz/biz_test.go go_test`
    - Add `//pkg/contextx` to the `deps` in `internal/app/domain/shortening/biz/b
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- internal/app/domain/shortening/biz/impl.go
+++ internal/app/domain/shortening/biz/impl.go
@@ -3,15 +3,21 @@ package biz
 import (
 	"github.com/blackhorseya/monorepo-go/entity/domain/shortening/biz"
 	"github.com/blackhorseya/monorepo-go/entity/domain/shortening/model"
+	"github.com/blackhorseya/monorepo-go/internal/app/domain/shortening/repo"
 	"github.com/blackhorseya/monorepo-go/pkg/contextx"
+	"go.uber.org/zap"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 type impl struct {
+	storage repo.Storager
 }
 
 // NewShortening is used to create a new shortening biz instance.
-func NewShortening() biz.IShorteningBiz {
-	return &impl{}
+func NewShortening(storage repo.Storager) biz.IShorteningBiz {
+	return &impl{
+		storage: storage,
+	}
 }
 
 func (i *impl) GetURLRecordByShortURL(ctx contextx.Contextx, shortURL string) (record *model.ShortenedUrl, err error) {
@@ -20,6 +26,19 @@ func (i *impl) GetURLRecordByShortURL(ctx contextx.Contextx, shortURL string) (r
 }
 
 func (i *impl) CreateShortenedURL(ctx contextx.Contextx, originalURL string) (record *model.ShortenedUrl, err error) {
-	// todo: 2024/1/9|sean|implement me
-	panic("implement me")
+	now := timestamppb.Now()
+	ret := &model.ShortenedUrl{
+		Id:          0,
+		OriginalUrl: originalURL,
+		ShortUrl:    originalURL,
+		CreatedAt:   now,
+		ExpiredAt:   nil,
+	}
+	err = i.storage.CreateURLRecord(ctx, ret)
+	if err != nil {
+		ctx.Error("create url record error", zap.Error(err), zap.Any("record", &ret))
+		return nil, err
+	}
+
+	return ret, nil
 }
--- internal/app/domain/shortening/biz/impl_test.go
+++ internal/app/domain/shortening/biz/impl_test.go
@@ -1,21 +1,100 @@
 package biz_test
 
 import (
+	"errors"
+	"reflect"
 	"testing"
 
+	shortB "github.com/blackhorseya/monorepo-go/entity/domain/shortening/biz"
+	"github.com/blackhorseya/monorepo-go/entity/domain/shortening/model"
+	"github.com/blackhorseya/monorepo-go/internal/app/domain/shortening/biz"
+	"github.com/blackhorseya/monorepo-go/internal/app/domain/shortening/repo"
+	"github.com/blackhorseya/monorepo-go/pkg/contextx"
 	"github.com/stretchr/testify/suite"
+	"go.uber.org/mock/gomock"
+	"go.uber.org/zap"
 )
 
 type suiteTester struct {
 	suite.Suite
+
+	logger  *zap.Logger
+	ctrl    *gomock.Controller
+	storage *repo.MockStorager
+	biz     shortB.IShorteningBiz
 }
 
 func (s *suiteTester) SetupTest() {
+	s.logger = zap.NewExample()
+	s.ctrl = gomock.NewController(s.T())
+	s.storage = repo.NewMockStorager(s.ctrl)
+	s.biz = biz.NewShortening(s.storage)
 }
 
 func (s *suiteTester) TearDownTest() {
+	s.ctrl.Finish()
 }
 
 func TestAll(t *testing.T) {
 	suite.Run(t, new(suiteTester))
+}
+
+func (s *suiteTester) Test_impl_CreateShortenedURL() {
+	longURL1 := "https://www.google.com"
+
+	type args struct {
+		ctx         contextx.Contextx
+		originalURL string
+		mock        func()
+	}
+	tests := []struct {
+		name       string
+		args       args
+		wantRecord *model.ShortenedUrl
+		wantErr    bool
+	}{
+		{
+			name: "create a record then error",
+			args: args{originalURL: longURL1, mock: func() {
+				s.storage.EXPECT().CreateURLRecord(gomock.Any(), gomock.Any()).Return(errors.New("error")).Times(1)
+			}},
+			wantRecord: nil,
+			wantErr:    true,
+		},
+		{
+			name: "ok",
+			args: args{originalURL: longURL1, mock: func() {
+				s.storage.EXPECT().CreateURLRecord(gomock.Any(), gomock.Any()).Return(nil).Times(1)
+			}},
+			wantRecord: &model.ShortenedUrl{
+				Id:          0,
+				OriginalUrl: longURL1,
+				ShortUrl:    longURL1,
+				CreatedAt:   nil,
+				ExpiredAt:   nil,
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		s.T().Run(tt.name, func(t *testing.T) {
+			tt.args.ctx = contextx.WithLogger(s.logger)
+			if tt.args.mock != nil {
+				tt.args.mock()
+			}
+
+			gotRecord, err := s.biz.CreateShortenedURL(tt.args.ctx, tt.args.originalURL)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("CreateShortenedURL() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if gotRecord != nil {
+				gotRecord.CreatedAt = nil
+				gotRecord.ExpiredAt = nil
+			}
+			if !reflect.DeepEqual(gotRecord, tt.wantRecord) {
+				t.Errorf("CreateShortenedURL() gotRecord = %v, want %v", gotRecord, tt.wantRecord)
+			}
+		})
+	}
 }
