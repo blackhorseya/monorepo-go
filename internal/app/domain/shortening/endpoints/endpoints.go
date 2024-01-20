d630507ac58ee9d98baf17cab56e3fe23b9d9e1c
Sean Zheng <blackhorseya@gmail.com>
2024-01-20 14:23:45 +0800

    feat: add new endpoints for URL shortening
    - Add new file: `internal/app/domain/shortening/endpoints/BUILD.bazel`
    - Add new file: `internal/app/domain/shortening/endpoints/endpoints.go`
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

---
+++ internal/app/domain/shortening/endpoints/endpoints.go
@@ -0,0 +1,35 @@
+package endpoints
+
+import (
+	"context"
+	"errors"
+
+	"github.com/blackhorseya/monorepo-go/entity/domain/shortening/biz"
+	"github.com/blackhorseya/monorepo-go/pkg/contextx"
+	responsex "github.com/blackhorseya/monorepo-go/pkg/response"
+	"github.com/go-kit/kit/endpoint"
+)
+
+// CreateShortURLRequest create short url request struct.
+type CreateShortURLRequest struct {
+	URL string `json:"url"`
+}
+
+// MakeCreateShortURLEndpoint returns an endpoint via the passed service.
+func MakeCreateShortURLEndpoint(svc biz.IShorteningBiz) endpoint.Endpoint {
+	return func(_ context.Context, request interface{}) (response interface{}, err error) {
+		ctx := contextx.Background()
+
+		req, ok := request.(CreateShortURLRequest)
+		if !ok {
+			return nil, errors.New("invalid request")
+		}
+
+		record, err := svc.CreateShortenedURL(ctx, req.URL)
+		if err != nil {
+			return responsex.Err.WrapError(err), nil
+		}
+
+		return responsex.OK.WithData(record), nil
+	}
+}
