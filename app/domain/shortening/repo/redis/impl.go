4a74f806435df5d43cd6a55b50f00b5927589b8b
Sean Zheng <blackhorseya@gmail.com>
2024-01-28 15:01:45 +0800

    feat: refactor variable and parameter names and implement URL record functions
    - Change the variable name `client` to `rw` in the `impl` struct
    - Change the parameter name `client` to `rw` in the `NewStorager` function
    - Implement the `GetURLRecordByShortURL` function
    - Implement the `CreateURLRecord` function
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- app/domain/shortening/repo/redis/impl.go
+++ app/domain/shortening/repo/redis/impl.go
@@ -1,6 +1,8 @@
 package redis
 
 import (
+	"encoding/json"
+
 	"github.com/blackhorseya/monorepo-go/app/domain/shortening/repo"
 	"github.com/blackhorseya/monorepo-go/entity/domain/shortening/model"
 	"github.com/blackhorseya/monorepo-go/pkg/contextx"
@@ -8,22 +10,41 @@ import (
 )
 
 type impl struct {
-	client *redis.Client
+	rw *redis.Client
 }
 
 // NewStorager is used to create a new redis storager instance.
-func NewStorager(client *redis.Client) (repo.Storager, error) {
+func NewStorager(rw *redis.Client) (repo.Storager, error) {
 	return &impl{
-		client: client,
+		rw: rw,
 	}, nil
 }
 
 func (i *impl) GetURLRecordByShortURL(ctx contextx.Contextx, shortURL string) (record *model.ShortenedUrl, err error) {
-	// todo: 2024/1/28|sean|implement me
-	panic("implement me")
+	result, err := i.rw.Get(ctx, shortURL).Result()
+	if err != nil {
+		return nil, err
+	}
+
+	var ret *model.ShortenedUrl
+	err = json.Unmarshal([]byte(result), &ret)
+	if err != nil {
+		return nil, err
+	}
+
+	return ret, nil
 }
 
 func (i *impl) CreateURLRecord(ctx contextx.Contextx, record *model.ShortenedUrl) error {
-	// todo: 2024/1/28|sean|implement me
-	panic("implement me")
+	marshal, err := json.Marshal(record)
+	if err != nil {
+		return err
+	}
+
+	err = i.rw.Set(ctx, record.ShortUrl, string(marshal), 0).Err()
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
