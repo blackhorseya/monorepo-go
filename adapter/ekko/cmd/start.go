3e181b82e2ca6f5a92421bf6aed865081282e623
Sean Zheng <blackhorseya@gmail.com>
2024-02-04 13:01:09 +0800

    feat: add start command to `ekko/cmd/adapter` package
    - Add `start.go` file to the `ekko/cmd` package in the `adapter` directory
    - Create `startCmd` variable with `start` command settings and a `Run` function that prints "start called"
    - Add `startCmd` to the `rootCmd` command
    - No changes to `BUILD.bazel` file
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

---
+++ adapter/ekko/cmd/start.go
@@ -0,0 +1,30 @@
+package cmd
+
+import (
+	"log"
+
+	"github.com/spf13/cobra"
+)
+
+// startCmd represents the start command
+var startCmd = &cobra.Command{
+	Use:   "start",
+	Short: "start a service",
+	Run: func(cmd *cobra.Command, args []string) {
+		log.Println("start a service")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(startCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// startCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// startCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
