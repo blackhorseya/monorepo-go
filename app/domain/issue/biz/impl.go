b771f2d2f2a4a9e7ae9925c688e6ba749b717a77
Sean Zheng <blackhorseya@gmail.com>
2024-02-04 13:56:33 +0800

    chore: refactor issue business logic and tests
    - Add `app/domain/issue/repo` as a dependency in `app/domain/issue/biz/BUILD.bazel`
    - Add `@org_uber_go_zap//:zap` as a dependency in `app/domain/issue/biz/BUILD.bazel`
    - Add `@org_uber_go_zap//:zap` as a dependency in `app/domain/issue/biz/impl.go`
    - Modify `NewIssueBiz` function in `app/domain/issue/biz/impl.go` to accept `storager` parameter
    - Modify `CreateTodo` function in `app/domain/issue/biz/impl.go` to call `storager.Create` and handle the returned value
    - Add `@org_uber_go_zap//:zap` as a dependency in `app/domain/issue/biz/impl_test.go`
    - Modify `suiteTester` struct in `app/domain/issue/biz/impl_test.go` to include a `mock` function parameter
    - Modify `SetupTest` function in `app/domain/issue/biz/impl_test.go` to replace the global zap logger with an example logger
    - Modify `NewIssueBiz` function in `app/domain/issue/biz/impl_test.go` to accept `storager` parameter
    - Add `Test_impl_CreateTodo` function in `app/domain/issue/biz/impl_test.go` to test the `CreateTodo` function in `impl.go`
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- app/domain/issue/biz/impl.go
+++ app/domain/issue/biz/impl.go
@@ -1,22 +1,32 @@
 package biz
 
 import (
+	"github.com/blackhorseya/monorepo-go/app/domain/issue/repo"
 	"github.com/blackhorseya/monorepo-go/entity/domain/issue/biz"
 	"github.com/blackhorseya/monorepo-go/entity/domain/issue/model"
 	"github.com/blackhorseya/monorepo-go/pkg/contextx"
+	"go.uber.org/zap"
 )
 
 type impl struct {
+	storager repo.Storager
 }
 
 // NewIssueBiz is used to create a new issue biz instance.
-func NewIssueBiz() (biz.IIssueBiz, error) {
-	return &impl{}, nil
+func NewIssueBiz(storager repo.Storager) (biz.IIssueBiz, error) {
+	return &impl{
+		storager: storager,
+	}, nil
 }
 
 func (i *impl) CreateTodo(ctx contextx.Contextx, title string) (todo *model.Ticket, err error) {
-	// todo: 2024/2/4|sean|implement me
-	panic("implement me")
+	ret, err := i.storager.Create(ctx, title)
+	if err != nil {
+		ctx.Error("create a todo into storage failed", zap.Error(err))
+		return nil, err
+	}
+
+	return ret, nil
 }
 
 func (i *impl) ListTodos(ctx contextx.Contextx) (todos []*model.Ticket, total int, err error) {
--- app/domain/issue/biz/impl_test.go
+++ app/domain/issue/biz/impl_test.go
@@ -1,12 +1,17 @@
 package biz
 
 import (
+	"errors"
+	"reflect"
 	"testing"
 
 	"github.com/blackhorseya/monorepo-go/app/domain/issue/repo"
 	"github.com/blackhorseya/monorepo-go/entity/domain/issue/biz"
+	"github.com/blackhorseya/monorepo-go/entity/domain/issue/model"
+	"github.com/blackhorseya/monorepo-go/pkg/contextx"
 	"github.com/stretchr/testify/suite"
 	"go.uber.org/mock/gomock"
+	"go.uber.org/zap"
 )
 
 type suiteTester struct {
@@ -18,9 +23,10 @@ type suiteTester struct {
 }
 
 func (s *suiteTester) SetupTest() {
+	zap.ReplaceGlobals(zap.NewExample())
 	s.ctrl = gomock.NewController(s.T())
 	s.storager = repo.NewMockStorager(s.ctrl)
-	issueBiz, err := NewIssueBiz()
+	issueBiz, err := NewIssueBiz(s.storager)
 	s.Require().NoError(err)
 	s.biz = issueBiz
 }
@@ -31,4 +37,47 @@ func (s *suiteTester) TearDownTest() {
 
 func TestAll(t *testing.T) {
 	suite.Run(t, new(suiteTester))
+}
+
+func (s *suiteTester) Test_impl_CreateTodo() {
+	type args struct {
+		ctx   contextx.Contextx
+		title string
+		mock  func()
+	}
+	tests := []struct {
+		name     string
+		args     args
+		wantTodo *model.Ticket
+		wantErr  bool
+	}{
+		{
+			name: "create todo by title then error",
+			args: args{title: "test", mock: func() {
+				s.storager.EXPECT().Create(gomock.Any(), "test").Return(
+					nil,
+					errors.New("mock error"),
+				).Times(1)
+			}},
+			wantTodo: nil,
+			wantErr:  true,
+		},
+	}
+	for _, tt := range tests {
+		s.T().Run(tt.name, func(t *testing.T) {
+			tt.args.ctx = contextx.Background()
+			if tt.args.mock != nil {
+				tt.args.mock()
+			}
+
+			gotTodo, err := s.biz.CreateTodo(tt.args.ctx, tt.args.title)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("CreateTodo() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(gotTodo, tt.wantTodo) {
+				t.Errorf("CreateTodo() gotTodo = %v, want %v", gotTodo, tt.wantTodo)
+			}
+		})
+	}
 }
