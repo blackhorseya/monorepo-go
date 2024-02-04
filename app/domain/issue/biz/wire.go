82da1204cc60845d67b1244135ccecc942165eea
Sean Zheng <blackhorseya@gmail.com>
2024-02-04 14:08:35 +0800

    feat: add MongoDB storage for issue domain
    - Add `//app/domain/issue/repo/mongodb` as a dependency in `app/domain/issue/biz/BUILD.bazel`
    - Import `github.com/blackhorseya/monorepo-go/app/domain/issue/repo/mongodb` in `app/domain/issue/biz/wire.go`
    - Add `mongodb.NewStorager` to the `ProvideDefaultSet` in `app/domain/issue/biz/wire.go`
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- app/domain/issue/biz/wire.go
+++ app/domain/issue/biz/wire.go
@@ -1,10 +1,12 @@
 package biz
 
 import (
+	"github.com/blackhorseya/monorepo-go/app/domain/issue/repo/mongodb"
 	"github.com/google/wire"
 )
 
 // ProvideDefaultSet is used to provide a default set for issue biz.
 var ProvideDefaultSet = wire.NewSet(
 	NewIssueBiz,
+	mongodb.NewStorager,
 )
