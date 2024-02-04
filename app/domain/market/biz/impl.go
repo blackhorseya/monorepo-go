4981cc59ced3cdf6365c5389e6a6fe6c2565c218
Sean Zheng <blackhorseya@gmail.com>
2024-02-04 16:24:01 +0800

    chore: add `pkg/finmindx` as a dependency and modify `impl.go` and `impl_test.go` in `app/domain/market/biz`
    - Add `pkg/finmindx` as a dependency in `app/domain/market/biz/BUILD.bazel`
    - Add `biz`, `entity/domain/market/biz`, `pkg/finmindx`, `@com_github_stretchr_testify//suite`, `@org_uber_go_mock//gomock`, and `@org_uber_go_zap//:zap` as dependencies in `app/domain/market/biz/BUILD.bazel`
    - Modify `app/domain/market/biz/impl.go` to include a new parameter `finmind` in the `NewMarketBiz` function
    - Modify `app/domain/market/biz/impl_test.go` to include new imports and initialize `finmind` in the `SetupTest` function
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- app/domain/market/biz/impl.go
+++ app/domain/market/biz/impl.go
@@ -4,14 +4,18 @@ import (
 	"github.com/blackhorseya/monorepo-go/entity/domain/market/biz"
 	"github.com/blackhorseya/monorepo-go/entity/domain/market/model"
 	"github.com/blackhorseya/monorepo-go/pkg/contextx"
+	"github.com/blackhorseya/monorepo-go/pkg/finmindx"
 )
 
 type impl struct {
+	finmind finmindx.Dialer
 }
 
 // NewMarketBiz is used to create a new market biz instance.
-func NewMarketBiz() (biz.IMarketBiz, error) {
-	return &impl{}, nil
+func NewMarketBiz(finmind finmindx.Dialer) (biz.IMarketBiz, error) {
+	return &impl{
+		finmind: finmind,
+	}, nil
 }
 
 func (i *impl) GetStockBySymbol(ctx contextx.Contextx, symbol string) (stock *model.Stock, err error) {
--- app/domain/market/biz/impl_test.go
+++ app/domain/market/biz/impl_test.go
@@ -3,17 +3,33 @@ package biz_test
 import (
 	"testing"
 
+	"github.com/blackhorseya/monorepo-go/app/domain/market/biz"
+	marketB "github.com/blackhorseya/monorepo-go/entity/domain/market/biz"
+	"github.com/blackhorseya/monorepo-go/pkg/finmindx"
 	"github.com/stretchr/testify/suite"
+	"go.uber.org/mock/gomock"
+	"go.uber.org/zap"
 )
 
 type suiteTester struct {
 	suite.Suite
+
+	ctrl    *gomock.Controller
+	finmind *finmindx.MockDialer
+	biz     marketB.IMarketBiz
 }
 
 func (s *suiteTester) SetupTest() {
+	zap.ReplaceGlobals(zap.NewExample())
+	s.ctrl = gomock.NewController(s.T())
+	s.finmind = finmindx.NewMockDialer(s.ctrl)
+	marketBiz, err := biz.NewMarketBiz(s.finmind)
+	s.NoError(err)
+	s.biz = marketBiz
 }
 
 func (s *suiteTester) TearDownTest() {
+	s.ctrl.Finish()
 }
 
 func TestAll(t *testing.T) {
