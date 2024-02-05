37e2e89f6b7a8f8c52d30bcc3ce0c18a3d805b97
Sean Zheng <blackhorseya@gmail.com>
2024-02-05 23:35:33 +0800

    feat: add restful service support to Orianna
    - Add `restful` as a dependency in `adapter/orianna/cmd/BUILD.bazel`
    - Add a new file `adapter/orianna/cmd/restful/BUILD.bazel`
    - Add a new file `adapter/orianna/cmd/restful/impl.go`
    - Add a new file `adapter/orianna/cmd/restful/wire.go`
    - Add a new file `adapter/orianna/cmd/restful/wire_gen.go`
    - Update `adapter/orianna/cmd/start.go` to include the `restful` service
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

---
+++ adapter/orianna/cmd/restful/wire.go
@@ -0,0 +1,17 @@
+//go:build wireinject
+
+//go:generate wire
+
+package restful
+
+import (
+	"github.com/blackhorseya/monorepo-go/pkg/adapterx"
+	"github.com/google/wire"
+	"github.com/spf13/viper"
+)
+
+func New(v *viper.Viper) (adapterx.Servicer, error) {
+	panic(wire.Build(
+		newRestful,
+	))
+}
