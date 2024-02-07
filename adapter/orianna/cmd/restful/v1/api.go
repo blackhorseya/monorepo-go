a593990f353f2b7635eb6a81054f53c31ec26fd1
Sean Zheng <blackhorseya@gmail.com>
2024-02-08 07:07:58 +0800

    feat: add new endpoint for retrieving stats information
    - Add a new endpoint `/v1/stats/info` with the method `GET` to get stats
    - Update the Swagger documentation for the new endpoint
    - Add a new package `stats` with its corresponding API file
    - Implement the `GetInfo` handler for the new endpoint in the `stats` package
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- adapter/orianna/cmd/restful/v1/api.go
+++ adapter/orianna/cmd/restful/v1/api.go
@@ -1,11 +1,13 @@
 package v1
 
 import (
+	"github.com/blackhorseya/monorepo-go/adapter/orianna/cmd/restful/v1/stats"
 	"github.com/blackhorseya/monorepo-go/adapter/orianna/cmd/restful/v1/stocks"
 	"github.com/gin-gonic/gin"
 )
 
 // Handle is the handler for the RESTful API.
 func Handle(g *gin.RouterGroup) {
+	stats.Handle(g.Group("/stats"))
 	stocks.Handle(g.Group("/stocks"))
 }
---
+++ adapter/orianna/cmd/restful/v1/stats/api.go
@@ -0,0 +1,31 @@
+package stats
+
+import (
+	"net/http"
+
+	"github.com/blackhorseya/monorepo-go/pkg/response"
+	"github.com/gin-gonic/gin"
+)
+
+type impl struct {
+}
+
+// Handle is the handler for the RESTful API.
+func Handle(g *gin.RouterGroup) {
+	instance := &impl{}
+
+	g.GET("/info", instance.GetInfo)
+}
+
+// GetInfo is the handler for the RESTful API.
+// @Summary Get Stats
+// @Description Get Stats
+// @Tags stats
+// @Accept json
+// @Produce json
+// @Success 200 {object} response.Response
+// @Router /v1/stats/info [get]
+func (i *impl) GetInfo(c *gin.Context) {
+	// todo: 2024/2/8|sean|implement the logic
+	c.JSON(http.StatusOK, response.Err.WithMessage("Not implemented"))
+}
