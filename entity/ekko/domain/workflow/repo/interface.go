6d7070c39dfb58f5a2b9ac2c842fe2fedfaeb69b
Sean Zheng <blackhorseya@gmail.com>
2024-02-10 06:24:31 +0800

    chore: add workflow repository interface and implementations
    - Add a new file `entity/ekko/domain/workflow/repo/BUILD.bazel`
    - Add a new file `entity/ekko/domain/workflow/repo/interface.go`
    - Add a new file `entity/ekko/domain/workflow/repo/mock_interface.go`
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

---
+++ entity/ekko/domain/workflow/repo/interface.go
@@ -0,0 +1,15 @@
+//go:generate mockgen -destination=./mock_${GOFILE} -package=${GOPACKAGE} -source=${GOFILE}
+
+package repo
+
+import (
+	"github.com/blackhorseya/monorepo-go/entity/ekko/domain/workflow/agg"
+	"github.com/blackhorseya/monorepo-go/pkg/contextx"
+)
+
+// IIssueRepo is the interface for issue repository.
+type IIssueRepo interface {
+	GetByID(ctx contextx.Contextx, id string) (issue agg.Issue, err error)
+	Create(ctx contextx.Contextx, item agg.Issue) error
+	Update(ctx contextx.Contextx, item agg.Issue) error
+}
