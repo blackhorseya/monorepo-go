515c524b96fb7a06e5b03a067d69ce474b26af02
Sean Zheng <blackhorseya@gmail.com>
2024-02-10 07:28:07 +0800

    chore: add `mongodb` and `wire` dependencies for workflow domain
    - Add `mongodb` and `wire` dependencies to `app/ekko/domain/workflow/biz/BUILD.bazel`
    - Import `github.com/blackhorseya/monorepo-go/app/ekko/domain/workflow/repo/mongodb` and `github.com/google/wire` in `app/ekko/domain/workflow/biz/wire.go`
    - Add `ProviderSet` as biz providers in `app/ekko/domain/workflow/biz/wire.go`
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- app/ekko/domain/workflow/biz/wire.go
+++ app/ekko/domain/workflow/biz/wire.go
@@ -1,1 +1,11 @@
 package biz
+
+import (
+	"github.com/blackhorseya/monorepo-go/app/ekko/domain/workflow/repo/mongodb"
+	"github.com/google/wire"
+)
+
+// ProviderSet is biz providers.
+var ProviderSet = wire.NewSet(
+	mongodb.NewIssueRepoWithMongoDB,
+)
