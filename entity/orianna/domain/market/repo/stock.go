6a508736f4b179dd8eaf6542a9460b2186177fcb
Sean Zheng <blackhorseya@gmail.com>
2024-02-11 09:54:34 +0800

    chore: add new files for market repository
    - Add new file `entity/orianna/domain/market/repo/BUILD.bazel`
    - Add new file `entity/orianna/domain/market/repo/mock_stock.go`
    - Add new file `entity/orianna/domain/market/repo/stock.go`
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

---
+++ entity/orianna/domain/market/repo/stock.go
@@ -0,0 +1,14 @@
+//go:generate mockgen -destination=./mock_${GOFILE} -package=${GOPACKAGE} -source=${GOFILE}
+
+package repo
+
+import (
+	"github.com/blackhorseya/monorepo-go/entity/orianna/domain/market/agg"
+	"github.com/blackhorseya/monorepo-go/pkg/contextx"
+)
+
+// IStockRepo is the interface for stock repository.
+type IStockRepo interface {
+	List(ctx contextx.Contextx) ([]agg.Stock, error)
+	Add(ctx contextx.Contextx, stock agg.Stock) error
+}
