ccf6d6a0e4ff28d285952672902054b39bef4081
Sean Zheng <blackhorseya@gmail.com>
2024-02-11 12:16:00 +0800

    test: add test for `GetStockBySymbol` function
    - Add a test for `GetStockBySymbol` function
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- app/orianna/domain/market/biz/impl_external_test.go
+++ app/orianna/domain/market/biz/impl_external_test.go
@@ -33,3 +33,27 @@ func TestImpl_ListStocks(t *testing.T) {
 
 	ctx.Debug("stocks", zap.Any("stocks", stocks))
 }
+
+func TestImpl_GetStockBySymbol(t *testing.T) {
+	zap.ReplaceGlobals(zap.NewExample())
+
+	err := configx.Load("", "sean")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	configx.ReplaceApplication(configx.C.Orianna)
+
+	biz, err := newForTest()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ctx := contextx.Background()
+	stock, err := biz.GetStockBySymbol(ctx, "2330")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ctx.Debug("stock", zap.Any("stock", stock))
+}
