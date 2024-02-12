c04786609880cbe7e7778e3b45788228659f0023
Sean Zheng <blackhorseya@gmail.com>
2024-02-12 11:51:32 +0800

    chore: remove the `stats` package and its contents from `adapter/orianna/cmd/restful`
    - Remove the `stats` dependency from the `v1` package in `adapter/orianna/cmd/restful`
    - Remove the `stats` package and its contents
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- adapter/orianna/cmd/restful/v1/api.go
+++ adapter/orianna/cmd/restful/v1/api.go
@@ -1,7 +1,6 @@
 package v1
 
 import (
-	"github.com/blackhorseya/monorepo-go/adapter/orianna/cmd/restful/v1/stats"
 	"github.com/blackhorseya/monorepo-go/adapter/orianna/cmd/restful/v1/stocks"
 	"github.com/blackhorseya/monorepo-go/entity/orianna/domain/market/biz"
 	"github.com/gin-gonic/gin"
@@ -9,6 +8,5 @@ import (
 
 // Handle is the handler for the RESTful API.
 func Handle(g *gin.RouterGroup, svc biz.IMarketBiz) {
-	stats.Handle(g.Group("/stats"))
 	stocks.Handle(g.Group("/stocks"), svc)
 }
