8dbcd3976271f922191a2ff3178d92f3a9437e22
Sean Zheng <blackhorseya@gmail.com>
2024-02-12 12:25:20 +0800

    feat: handle callback and Line bot messages for Orianna RESTful API
    - Modify `impl.go` in `adapter/orianna/cmd/restful/`:
      - Add code to handle the callback endpoint
      - Add code to handle messages from Line bot
    - Modify `BUILD.bazel` in `entity/orianna/domain/market/agg/`:
      - Add a dependency on `linebot` package
    - Modify `stock.go` in `entity/orianna/domain/market/agg/`:
      - Add a method to create a flex message for stock info
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- adapter/orianna/cmd/restful/impl.go
+++ adapter/orianna/cmd/restful/impl.go
@@ -1,6 +1,7 @@
 package restful
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 	"os"
@@ -116,5 +117,73 @@ func (i *impl) healthz(c *gin.Context) {
 // @Failure 500 {object} response.Response
 // @Router /callback [post]
 func (i *impl) callback(c *gin.Context) {
-	panic("implement me")
+	ctx, err := contextx.FromGin(c)
+	if err != nil {
+		_ = c.Error(err)
+		return
+	}
+
+	events, err := i.bot.ParseRequest(c.Request)
+	if err != nil {
+		if errors.Is(err, linebot.ErrInvalidSignature) {
+			ctx.Error("invalid line bot signature", zap.Error(err))
+			_ = c.Error(err)
+		} else {
+			ctx.Error("parse line bot request error", zap.Error(err))
+			_ = c.Error(err)
+		}
+
+		return
+	}
+
+	var messages []linebot.SendingMessage
+	for _, event := range events {
+		if event.Type == linebot.EventTypeMessage {
+			message, ok := event.Message.(*linebot.TextMessage)
+			if !ok {
+				continue
+			}
+
+			messages, err = i.handleMessage(ctx, message)
+			if err != nil {
+				ctx.Warn("handle message error", zap.Error(err))
+				continue
+			}
+
+			_, err = i.bot.ReplyMessage(event.ReplyToken, messages...).Do()
+			if err != nil {
+				_ = c.Error(err)
+				return
+			}
+		}
+	}
+
+	c.Status(http.StatusOK)
+}
+
+func (i *impl) handleMessage(ctx contextx.Contextx, message *linebot.TextMessage) ([]linebot.SendingMessage, error) {
+	text := message.Text
+	if text == "ping" {
+		return []linebot.SendingMessage{
+			linebot.NewTextMessage("pong"),
+		}, nil
+	}
+
+	split := strings.Split(message.Text, ".")
+	if len(split) == 2 && split[0] == "q" {
+		symbol := split[1]
+
+		stock, err := i.svc.GetStockBySymbol(ctx, symbol)
+		if err != nil {
+			return nil, err
+		}
+
+		return []linebot.SendingMessage{
+			stock.FlexMessage(),
+		}, nil
+	}
+
+	return []linebot.SendingMessage{
+		linebot.NewTextMessage("I don't understand what you said."),
+	}, nil
 }
--- entity/orianna/domain/market/agg/stock.go
+++ entity/orianna/domain/market/agg/stock.go
@@ -2,8 +2,10 @@ package agg
 
 import (
 	"encoding/json"
+	"strconv"
 
 	"github.com/blackhorseya/monorepo-go/entity/orianna/domain/market/model"
+	"github.com/line/line-bot-sdk-go/v8/linebot"
 )
 
 // Stock is an aggregate root that represents the stock.
@@ -71,4 +73,56 @@ func (x *Stock) GetExchangeName() string {
 
 func (x *Stock) GetRecentQuota() model.StockQuota {
 	return x.recentQuota
+}
+
+// FlexMessage is used to create a flex message.
+func (x *Stock) FlexMessage() *linebot.FlexMessage {
+	return linebot.NewFlexMessage("StockInfo", &linebot.BubbleContainer{
+		Type: linebot.FlexContainerTypeBubble,
+		Body: &linebot.BoxComponent{
+			Type:   linebot.FlexComponentTypeBox,
+			Layout: linebot.FlexBoxLayoutTypeVertical,
+			Contents: []linebot.FlexComponent{
+				&linebot.TextComponent{
+					Type:   linebot.FlexComponentTypeText,
+					Text:   x.stock.Symbol + " " + x.stock.Name,
+					Weight: linebot.FlexTextWeightTypeBold,
+					Size:   linebot.FlexTextSizeTypeXxl,
+					Margin: linebot.FlexComponentMarginTypeMd,
+				},
+				&linebot.SeparatorComponent{
+					Type:   linebot.FlexComponentTypeSeparator,
+					Margin: linebot.FlexComponentMarginTypeXxl,
+				},
+				&linebot.BoxComponent{
+					Type:    linebot.FlexComponentTypeBox,
+					Layout:  linebot.FlexBoxLayoutTypeVertical,
+					Margin:  linebot.FlexComponentMarginTypeXxl,
+					Spacing: linebot.FlexComponentSpacingTypeSm,
+					Contents: []linebot.FlexComponent{
+						&linebot.BoxComponent{
+							Type:   linebot.FlexComponentTypeBox,
+							Layout: linebot.FlexBoxLayoutTypeHorizontal,
+							Contents: []linebot.FlexComponent{
+								&linebot.TextComponent{
+									Type:  linebot.FlexComponentTypeText,
+									Text:  "Price",
+									Size:  linebot.FlexTextSizeTypeSm,
+									Color: "#555555",
+									Flex:  linebot.IntPtr(0),
+								},
+								&linebot.TextComponent{
+									Type:  linebot.FlexComponentTypeText,
+									Text:  "$" + strconv.FormatFloat(x.recentQuota.GetClose(), 'f', 2, 64),
+									Size:  linebot.FlexTextSizeTypeSm,
+									Color: "#111111",
+									Align: linebot.FlexComponentAlignTypeEnd,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	})
 }
