64f37d4653f887f3b7f929b67da54ac758ceabb1
Sean Zheng <blackhorseya@gmail.com>
2024-02-17 22:24:55 +0800

    feat: create new files for strategy repo in entity/orianna domain
    - Add a new file `BUILD.bazel` under `entity/orianna/domain/strategy/repo`
    - Add a new file `mock_strategy.go` under `entity/orianna/domain/strategy/repo`
    - Add a new file `strategy.go` under `entity/orianna/domain/strategy/repo`
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

---
+++ entity/orianna/domain/strategy/repo/strategy.go
@@ -0,0 +1,14 @@
+//go:generate mockgen -destination=./mock_${GOFILE} -package=${GOPACKAGE} -source=${GOFILE}
+
+package repo
+
+import (
+	"github.com/blackhorseya/monorepo-go/entity/orianna/domain/strategy/agg"
+	"github.com/blackhorseya/monorepo-go/pkg/contextx"
+)
+
+// IStrategyRepo is the interface for strategy repository.
+type IStrategyRepo interface {
+	Get(ctx contextx.Contextx, id string) (agg.Strategy, error)
+	Create(ctx contextx.Contextx, data agg.Strategy) error
+}
