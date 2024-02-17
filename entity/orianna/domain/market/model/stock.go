53d43bc63cf46f567377ace13510adc880d70259
Sean Zheng <blackhorseya@gmail.com>
2024-02-17 11:55:25 +0800

    refactor: update Stock struct fields to be optional
    - Update `Name`, `IndustryCategory`, and `ExchangeName` fields in the `Stock` struct to be optional
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- entity/orianna/domain/market/model/stock.go
+++ entity/orianna/domain/market/model/stock.go
@@ -3,7 +3,7 @@ package model
 // Stock is an entity that represents the stock.
 type Stock struct {
 	Symbol           string `json:"symbol"`
-	Name             string `json:"name"`
-	IndustryCategory string `json:"industry_category"`
-	ExchangeName     string `json:"exchange_name"`
+	Name             string `json:"name,omitempty"`
+	IndustryCategory string `json:"industry_category,omitempty"`
+	ExchangeName     string `json:"exchange_name,omitempty"`
 }
