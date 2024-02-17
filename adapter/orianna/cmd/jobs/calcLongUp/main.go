8355e2b617ef765f9cc7dc63cba4b531eb51a5cf
Sean Zheng <blackhorseya@gmail.com>
2024-02-17 11:24:21 +0800

    feat: integrate new `calcLongUp` function into project
    - Add a new `BUILD.bazel` file for the `calcLongUp` job
    - Add a new `main.go` file for the `calcLongUp` job
    - Add a new `wire.go` file for the `calcLongUp` job
    - Add a new `wire_gen.go` file for the `calcLongUp` job
    - Update the `serverless.yml` file to include the `calcLongUp` function
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

---
+++ adapter/orianna/cmd/jobs/calcLongUp/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/blackhorseya/monorepo-go/pkg/configx"
+	"github.com/blackhorseya/monorepo-go/pkg/contextx"
+	"github.com/blackhorseya/monorepo-go/pkg/logging"
+	"go.uber.org/zap"
+)
+
+var (
+	injector *Injector
+)
+
+// Response is of type APIGatewayProxyResponse since we're leveraging the
+// AWS Lambda Proxy Request functionality (default behavior)
+//
+// https://serverless.com/framework/docs/providers/aws/events/apigateway/#lambda-proxy-integration
+type Response events.APIGatewayProxyResponse
+
+// Handler is our lambda handler invoked by the `lambda.Start` function call
+func Handler(c context.Context) (Response, error) {
+	return Response{StatusCode: http.StatusOK}, nil
+}
+
+func main() {
+	err := configx.Load("", "sean")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	configx.ReplaceApplication(configx.C.Orianna)
+
+	err = logging.InitWithConfig(configx.C.Log)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	injector, err = BuildInjector()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	lambda.Start(Handler)
+}
+
+func handleError(err error) (Response, error) {
+	ctx := contextx.Background()
+
+	if injector.notifier != nil {
+		_ = injector.notifier.SendText(ctx, fmt.Sprintf("[CalcLongUp] failed to execute the job: %v", err))
+	}
+
+	ctx.Error("failed to execute the job", zap.Error(err))
+
+	return Response{StatusCode: http.StatusInternalServerError}, err
+}
