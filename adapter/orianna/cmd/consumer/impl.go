9b79c436443388133cead548d224eaa02be6b1cc
Sean Zheng <blackhorseya@gmail.com>
2024-02-17 12:15:24 +0800

    feat: implement signal handling and logging in package
    - Add `pkg/contextx` as a dependency in `BUILD.bazel`
    - Import necessary packages for signals and time in `impl.go`
    - Initialize `done` channel in `impl` struct
    - Implement a basic logging functionality in `Start` method
    - Implement signal handling and logging in `AwaitSignal` method
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- adapter/orianna/cmd/consumer/impl.go
+++ adapter/orianna/cmd/consumer/impl.go
@@ -1,10 +1,18 @@
 package consumer
 
 import (
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
 	"github.com/blackhorseya/monorepo-go/pkg/adapterx"
+	"github.com/blackhorseya/monorepo-go/pkg/contextx"
+	"go.uber.org/zap"
 )
 
 type impl struct {
+	done chan struct{}
 }
 
 func newConsumer() (adapterx.Servicer, error) {
@@ -12,11 +20,28 @@ func newConsumer() (adapterx.Servicer, error) {
 }
 
 func (i *impl) Start() error {
-	// TODO implement me
-	panic("implement me")
+	ctx := contextx.Background()
+
+	go func() {
+		for {
+			ctx.Info("consumer is running")
+			time.Sleep(5 * time.Second)
+		}
+	}()
+
+	return nil
 }
 
 func (i *impl) AwaitSignal() error {
-	// TODO implement me
-	panic("implement me")
+	c := make(chan os.Signal, 1)
+	signal.Reset(syscall.SIGTERM, syscall.SIGINT)
+	signal.Notify(c, syscall.SIGTERM, syscall.SIGINT)
+
+	if sig := <-c; true {
+		ctx := contextx.Background()
+		ctx.Info("receive signal", zap.String("signal", sig.String()))
+	}
+
+	return nil
+
 }
