d36dc119fdf4c4efd493d29799e229f29f5bc6ea
Sean Zheng <blackhorseya@gmail.com>
2024-02-23 06:59:00 +0800

    chore: refactor `cmd/lambda` initialization and configuration
    - Add dependencies to `BUILD.bazel` for `cmd/lambda`
    - Import new packages in `main.go` for `cmd/lambda`
    - Replace hardcoded values with configuration values in `main.go`
    - Initialize logging configuration in `main.go`
    - Initialize `restful` application in `main.go`
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- adapter/reurl/cmd/lambda/main.go
+++ adapter/reurl/cmd/lambda/main.go
@@ -2,15 +2,47 @@ package main
 
 import (
 	"context"
+	"log"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	ginadapter "github.com/awslabs/aws-lambda-go-api-proxy/gin"
+	"github.com/blackhorseya/monorepo-go/adapter/reurl/cmd/restful"
+	"github.com/blackhorseya/monorepo-go/pkg/configx"
+	"github.com/blackhorseya/monorepo-go/pkg/logging"
+	"github.com/spf13/viper"
 )
 
+var ginLambda *ginadapter.GinLambda
+
 func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	return events.APIGatewayProxyResponse{StatusCode: 200}, nil
+	return ginLambda.ProxyWithContext(ctx, req)
 }
 
 func main() {
+	err := configx.Load("", "sean")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	configx.ReplaceApplication(configx.C.ReURL)
+
+	err = logging.InitWithConfig(configx.C.Log)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	app, err := restful.NewRestful(viper.GetViper())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = app.InitRouting()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ginLambda = ginadapter.New(app.GetRouter())
+
 	lambda.Start(Handler)
 }
