20d552aafb3d942fb17716a89c04c18721ce76c1
Sean Zheng <blackhorseya@gmail.com>
2024-02-28 10:01:00 +0800

    chore: update dependencies and error handling in main file
    - Add `pkg/contextx` and `go.uber.org/zap` dependencies to `BUILD.bazel` file
    - Add error handling and HTTP client request in `main.go` file
    - Remove redundant `Body` assignment in `Handler` function
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- adapter/orianna/cmd/jobs/fetchEarningsCall/main.go
+++ adapter/orianna/cmd/jobs/fetchEarningsCall/main.go
@@ -1,18 +1,62 @@
 package main
 
 import (
+	"errors"
+	"io"
 	"log"
+	"net/http"
+	"net/url"
+	"strings"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/blackhorseya/monorepo-go/pkg/configx"
+	"github.com/blackhorseya/monorepo-go/pkg/contextx"
 	"github.com/blackhorseya/monorepo-go/pkg/logging"
+	"go.uber.org/zap"
+)
+
+const (
+	baseURL = "https://mops.twse.com.tw/mops/web/ajax_t100sb02_1"
 )
 
 func Handler() (events.APIGatewayProxyResponse, error) {
+	ctx := contextx.Background()
+
+	payload := url.Values{}
+	payload.Set("encodeURIComponent", "1")
+	payload.Set("step", "1")
+	payload.Set("firstin", "1")
+	payload.Set("off", "1")
+	payload.Set("TYPEK", "sii")
+	payload.Set("year", "113")
+	payload.Set("month", "02")
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, baseURL, strings.NewReader(payload.Encode()))
+	if err != nil {
+		return events.APIGatewayProxyResponse{}, err
+	}
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return events.APIGatewayProxyResponse{}, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return events.APIGatewayProxyResponse{}, errors.New("status code is not 200, got " + resp.Status)
+	}
+
+	got, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return events.APIGatewayProxyResponse{}, err
+	}
+
+	ctx.Info("got", zap.String("body", string(got)))
+
 	return events.APIGatewayProxyResponse{
 		StatusCode: 200,
-		Body:       "Hello, World!",
 	}, nil
 }
 
