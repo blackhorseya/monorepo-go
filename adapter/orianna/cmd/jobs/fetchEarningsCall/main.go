978173bc4df42c9d251fe08e9c7eb7adab31c7c5
Sean Zheng <blackhorseya@gmail.com>
2024-02-28 10:48:19 +0800

    feat: refactor struct tags and add new timex converter function
    - Add `pkg/timex/converter.go` to the `timex` library
    - Modify the `EarningsCall` struct tags to include `omitempty` for fields
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- adapter/orianna/cmd/jobs/fetchEarningsCall/main.go
+++ adapter/orianna/cmd/jobs/fetchEarningsCall/main.go
@@ -2,18 +2,27 @@ package main
 
 import (
 	"log"
+	"strings"
+	"time"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/blackhorseya/monorepo-go/entity/orianna/domain/market/model"
 	"github.com/blackhorseya/monorepo-go/pkg/configx"
 	"github.com/blackhorseya/monorepo-go/pkg/contextx"
 	"github.com/blackhorseya/monorepo-go/pkg/logging"
+	"github.com/blackhorseya/monorepo-go/pkg/timex"
 	"github.com/gocolly/colly/v2"
+	"github.com/google/uuid"
 	"go.uber.org/zap"
 )
 
 const (
 	baseURL = "https://mops.twse.com.tw/mops/web/ajax_t100sb02_1"
+)
+
+var (
+	loc, _ = time.LoadLocation("Asia/Taipei")
 )
 
 func Handler() (events.APIGatewayProxyResponse, error) {
@@ -27,7 +36,44 @@ func Handler() (events.APIGatewayProxyResponse, error) {
 
 	c.OnHTML("#myTable > tbody", func(e *colly.HTMLElement) {
 		e.ForEach("tr", func(i int, tr *colly.HTMLElement) {
-			ctx.Info("fetching", zap.String("symbol", tr.ChildText("td:nth-child(1)")))
+			split := strings.Split(tr.ChildText("td:nth-child(3)"), "至")
+			if len(split) == 0 {
+				ctx.Error(
+					"split date string error",
+					zap.String("text", tr.ChildText("td:nth-child(3)")),
+				)
+				return
+			}
+			atDate, err := time.ParseInLocation(
+				"2006/01/02",
+				timex.FormatTaiwanDate(strings.TrimSpace(split[len(split)-1])),
+				loc,
+			)
+			if err != nil {
+				ctx.Error(
+					"parse date error",
+					zap.Error(err),
+					zap.String("text", tr.ChildText("td:nth-child(3)")),
+				)
+				return
+			}
+
+			timeStr := strings.ReplaceAll(tr.ChildText("td:nth-child(4)"), " ", "")
+			atTime, err := time.ParseInLocation("15:04", timeStr, loc)
+			if err != nil {
+				ctx.Error("parse time error", zap.Error(err))
+				return
+			}
+
+			at := time.Date(atDate.Year(), atDate.Month(), atDate.Day(), atTime.Hour(), atTime.Minute(), 0, 0, loc)
+
+			call := model.EarningsCall{
+				ID:         uuid.New(),
+				Symbol:     tr.ChildText("td:nth-child(1)"),
+				Host:       "",
+				OccurredAt: at,
+			}
+			ctx.Info("fetching", zap.Any("call", call))
 		})
 	})
 
--- entity/orianna/domain/market/model/earnings_call.go
+++ entity/orianna/domain/market/model/earnings_call.go
@@ -8,8 +8,8 @@ import (
 
 // EarningsCall is an entity that represents an earnings call
 type EarningsCall struct {
-	ID         uuid.UUID `json:"id"`
-	Symbol     string    `json:"symbol"`
-	Host       string    `json:"host"`
-	OccurredAt time.Time `json:"occurred_at"`
+	ID         uuid.UUID `json:"id,omitempty"`
+	Symbol     string    `json:"symbol,omitempty"`
+	Host       string    `json:"host,omitempty"`
+	OccurredAt time.Time `json:"occurred_at,omitempty"`
 }
