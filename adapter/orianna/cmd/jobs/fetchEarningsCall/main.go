8d1af7fd20131c2861b6dc6619452f3b137d6853
Sean Zheng <blackhorseya@gmail.com>
2024-02-28 09:38:14 +0800

    feat: introduce serverless function for fetching earnings calls
    - Add a new file `BUILD.bazel` with rules for building Go binaries
    - Add a new file `main.go` with a basic lambda handler function
    - Update `serverless.yml` to include a new function `fetchEarningsCall` scheduled to run at a specific time
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

---
+++ adapter/orianna/cmd/jobs/fetchEarningsCall/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/blackhorseya/monorepo-go/pkg/configx"
+	"github.com/blackhorseya/monorepo-go/pkg/logging"
+)
+
+func Handler() (events.APIGatewayProxyResponse, error) {
+	return events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       "Hello, World!",
+	}, nil
+}
+
+func main() {
+	err := configx.Load("", "sean")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	configx.ReplaceApplication(configx.C.Orianna)
+
+	err = logging.InitWithConfig(configx.C.Log)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	lambda.Start(Handler)
+}
