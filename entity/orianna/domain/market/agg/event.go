e8403ab4712ac0bf59bdfdd6f684bf02a07b1de2
Sean Zheng <blackhorseya@gmail.com>
2024-03-01 21:51:34 +0800

    feat: refactor event handling and model integration
    - Import the `model` package in `event.go`
    - Embed the `model.Event` in the `Event` struct
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- entity/orianna/domain/market/agg/event.go
+++ entity/orianna/domain/market/agg/event.go
@@ -1,5 +1,10 @@
 package agg
 
+import (
+	"github.com/blackhorseya/monorepo-go/entity/orianna/domain/market/model"
+)
+
 // Event is an aggregate root that represents the event.
 type Event struct {
+	*model.Event
 }
